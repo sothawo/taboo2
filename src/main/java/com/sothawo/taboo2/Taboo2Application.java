a28b7e3b05571c0028ec52e55c63508812121be6
P.J. Meisch <pj.meisch@sothawo.com>
2015-09-15 19:30:32 +0200

    added basic program files for spring-boot service

---
+++ src/main/java/com/sothawo/taboo2/Taboo2Application.java
@@ -0,0 +1,12 @@
+package com.sothawo.taboo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taboo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taboo2Application.class, args);
+    }
+}
---
+++ src/test/java/com/sothawo/taboo2/Taboo2ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.sothawo.taboo2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Taboo2Application.class)
+@WebAppConfiguration
+public class Taboo2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
