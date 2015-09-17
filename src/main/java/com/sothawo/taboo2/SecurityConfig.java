a44a2f558a827612cab79bfb694dbda106b34830
P.J. Meisch <pj.meisch@sothawo.com>
2015-09-17 22:46:40 +0200

    https-only is based on configuration value

--- src/main/java/com/sothawo/taboo2/SecurityConfig.java
+++ src/main/java/com/sothawo/taboo2/SecurityConfig.java
@@ -5,6 +5,8 @@
  */
 package com.sothawo.taboo2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,9 +21,14 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    SecurityProperties securityProperties;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requiresChannel().anyRequest().requiresSecure();
+        if (securityProperties.isRequireSsl()) {
+            http.requiresChannel().anyRequest().requiresSecure();
+        }
         http.csrf().disable();
     }
 }
